0c83fd97bf3db67a5fc7fc5d1281157a38b90f27
SethyPagna <ungsethypagna30@gmail.com>
2026-05-13 02:28:56 +0800

    chore: harden next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  poweredByHeader: false,
   eslint: {
     ignoreDuringBuilds: true,
   },
   images: {
     remotePatterns: [{ protocol: "https", hostname: "**" }],
-  },
-  experimental: {
-    serverActions: { allowedOrigins: ["*"] },
   },
 };
 
