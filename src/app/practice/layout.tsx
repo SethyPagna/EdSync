616090a477a9f0811b0a2ce2b5136b69dbd87557
SethyPagna <ungsethypagna30@gmail.com>
2026-05-19 07:13:57 +0800

    wrap practice route in workspace shell

---
+++ src/app/practice/layout.tsx
@@ -0,0 +1,5 @@
+import SharedWorkspaceShell from "@/components/SharedWorkspaceShell";
+
+export default function PracticeLayout({ children }: { children: React.ReactNode }) {
+  return <SharedWorkspaceShell>{children}</SharedWorkspaceShell>;
+}
