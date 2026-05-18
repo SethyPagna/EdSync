55f7dae196f97a2349b4473bdbdead24c386cae2
SethyPagna <ungsethypagna30@gmail.com>
2026-05-19 07:19:14 +0800

    wrap quizzes route in workspace shell

---
+++ src/app/quizzes/layout.tsx
@@ -0,0 +1,5 @@
+import SharedWorkspaceShell from "@/components/SharedWorkspaceShell";
+
+export default function QuizzesLayout({ children }: { children: React.ReactNode }) {
+  return <SharedWorkspaceShell>{children}</SharedWorkspaceShell>;
+}
