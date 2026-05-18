acf74bf1377f7086619a122903969800686fc2fd
SethyPagna <ungsethypagna30@gmail.com>
2026-05-19 07:20:22 +0800

    wrap games route in workspace shell

---
+++ src/app/games/layout.tsx
@@ -0,0 +1,5 @@
+import SharedWorkspaceShell from "@/components/SharedWorkspaceShell";
+
+export default function GamesLayout({ children }: { children: React.ReactNode }) {
+  return <SharedWorkspaceShell>{children}</SharedWorkspaceShell>;
+}
