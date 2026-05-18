68cad1eda3fef2ceb5e6fba7628f4eb9cf4e99b6
SethyPagna <ungsethypagna30@gmail.com>
2026-05-19 07:14:57 +0800

    wrap notes route in workspace shell

---
+++ src/app/notes/layout.tsx
@@ -0,0 +1,5 @@
+import SharedWorkspaceShell from "@/components/SharedWorkspaceShell";
+
+export default function NotesLayout({ children }: { children: React.ReactNode }) {
+  return <SharedWorkspaceShell>{children}</SharedWorkspaceShell>;
+}
