d2eb475c361647cc147f141b9d343d91e1aee48e
SethyPagna <ungsethypagna30@gmail.com>
2026-05-28 14:51:27 +0800

    Add grades module index

---
+++ src/lib/grades/index.ts
@@ -0,0 +1,33 @@
+export const GRADE_LEVELS = [
+  "Pre-K",
+  "Kindergarten",
+  "Grade 1",
+  "Grade 2",
+  "Grade 3",
+  "Grade 4",
+  "Grade 5",
+  "Grade 6",
+  "Grade 7",
+  "Grade 8",
+  "Grade 9",
+  "Grade 10",
+  "Grade 11",
+  "Grade 12",
+  "College",
+  "Adult Learning",
+] as const;
+
+export const SUBJECT_AREAS = [
+  "English Language Arts",
+  "Mathematics",
+  "Science",
+  "Social Studies",
+  "Computer Science",
+  "World Languages",
+  "Art & Design",
+  "Music",
+  "Health",
+  "Physical Education",
+  "Business",
+  "Career & Technical Education",
+] as const;
